5f6ad1dfec1c7ea1d4702dc1da12a8a4afb63780
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 16:34:43 -0700

    Starting to update variable names

--- wordcount.py
+++ wordcount.py
@@ -3,29 +3,29 @@
 open_text = open('test.txt')
 
 def word_count():
-    word_dict = {}
+    unique_count = {}
     all_words = []
     for line in open_text:
         words_on_line = line.rstrip(). split(" ")
         for word in words_on_line:
             all_words.append(word)
-            if word not in word_dict:
-                word_dict[word] = 0
+            if word not in unique_count:
+                unique_count[word] = 0
 
     print "Original Dictionary:"
-    print word_dict    
+    print unique_count    
     # tests passed:
-    # print word_dict
+    # print unique_count
     # print "    "
     # print all_words
     for word in all_words:
-        if word in word_dict:
-            word_dict[word] += 1
+        if word in unique_count:
+            unique_count[word] += 1
 
     print "\nThis is the new dictionary with count:"
     
-    for word in word_dict:
-        print word, word_dict[word]
+    for word in unique_count:
+        print word, unique_count[word]
 
     #d([key] = value
 
